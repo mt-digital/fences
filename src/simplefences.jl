f2fc4eb91c52e9a020c81f1f61d694f08f508de7
Matthew Turner <maturner01@gmail.com>
2024-02-23 12:20:54 -0800

    nearly done filling out agent_step

--- src/simplefences.jl
+++ src/simplefences.jl
@@ -3,18 +3,26 @@
 # 
 # 
 using Agents
+using Memoize
+using Iterators: filter
 
 
 @enum Species insect bird herbivore carnivore livestock
 
 # Do not list cattle 
 default_birth_rates = 
-    Dict(insect => 0.2, bird => 0.05, herbivore => 0.02, carnivore => 0.01)
+    Dict(insect => 0.2, bird => 0.1, herbivore => 0.05, livestock => 0.05, carnivore => 0.02)
 
-    prey_lookup => Dict(insect => [], bird => [insect], herbivore => [], 
-                        carnivore => [herbivore])
+default_init_energy = 
+    Dict(insect => 0.1, bird => 0.4, herbivore => 1, livestock => 2, carnivore => 3)
 
-@agent Critter GridAgent{2} begin
+default_Δenergy = 
+   Dict(insect => .01, bird => 0.04, herbivore => 0.1, livestock => 0.2, carnivore => 0.3)
+
+prey_lookup = Dict(insect => [], bird => [insect], herbivore => [], 
+                   livestock => [], carnivore => [herbivore])
+
+@with_kw @agent Critter GridAgent{2} begin
 
     energy::Float64
     Δenergy::Float64
@@ -24,9 +32,96 @@ default_birth_rates =
     birth_rate::Union{Float64,Function}
 
     species::Species
+    landowner::Union{Landholder,Nothing} = nothing
+
 end
 
 
+# Handle mortal dynamics (use birth rates for each agent for new births and 
+# check for energy <= 0.0 for die-off). Grow vegetation depending on presence
+# of pollinators (insects).
+function model_step!(model)
+
+    for pos in positions(model)
+        # Grow vegetation depending on insects.
+        
+    end
+
+end
+
+#: Helper function for agent_step to find and eat available prey at agent position.
+function eat_available_prey!(agent, model)
+    if agent.species == carnivore
+        prey_species = [herbivore, cattle]
+    elseif agent.species == bird
+        prey_species = [insect]
+    else
+        error("agent species, ${agent.species} has no known prey animals")
+    end
+
+    # find available prey
+    available_prey = filter(a -> a.species ∈ prey_species,
+                            agents_in_position(agent.pos, model))
+
+    # take one of the available prey at random if any are available
+    if !isempty(available_prey)
+        prey = rand(available_prey)
+        agent.energy += prey.energy
+        remove_agent!(prey, model)
+    end
+
+end
+
+# Handle movement, feeding. 
+function agent_step!(agent, model)
+
+    ## Move to a location depending on species and state...
+    # ...livestock go to random location in landowner's holding...
+    if agent.species == livestock
+        new_pos = rand(agent.landowner.holding) 
+        move_agent!(agent, new_pos, model)
+
+    # ...birds and insects can random-walk anywhere (1 cell by default)...
+    elseif agent.species ∈ [bird, insect]
+        new_pos = rand(nearby_positions(agent.pos, model))
+
+    # ...finally, herbivores and carnivores can't go to fenced locations.
+    else
+        new_pos = filter(pos -> check_pos_available(pos, model), 
+                         nearby_positions(agent.pos, model))
+    end
+    move_agent!(agent, new_pos, model)
+
+    # Feed, depending on species, location, and location's state...
+    # ...if not a carnivore or bird, eat grass at location up to 4*Δenergy...
+    if agent.species ∈ [insect, herbivore, livestock]
+        grass_avail = grass[agent.pos]
+        Δenergy = agent.Δenergy
+        energy_to_eat = min(4*Δenergy, grass_avail)
+        grass_avail -= energy_to_eat
+
+    # ...if it is a carnivore, try to eat herbivore or livestock at location...
+    else
+        eat_available_prey!(agent, model)
+    
+    end
+
+    # Lose Δenergy required to live for one time step.
+    agent.energy -= agent.Δenergy
+
+    # Die off if energy is negative. 
+    if agent.energy ≤ 0
+        remove_agent!(agent, model)
+    end
+
+end
+
+
+@memoize function check_pos_available(pos, model)
+
+   return pos ∈ model.available_cells
+
+end
 
 
 @with_kw mutable struct Landholder begin
@@ -35,19 +130,19 @@ end
     holding::Array{Tuple{Int,Int}}
     livestock::Array{Int} = []
     fence_probability::Function = 0.5
-
+    fenced::Bool = false
 end
 
 
-function livestock_factory!(model, holding; 
+function livestock_factory!(model, landholder; 
                            default_energy = 10, default_Δenergy = 0.1)
 
     # Select random point in holding to place cattle.
-    pos = rand(holding)
+    pos = rand(landholder.holding)
 
     # Create and return new livestock Critter.
     return add_agent!(pos, model, default_energy, default_Δenergy, 
-                      grass_frac -> 0.2 * grass_frac, livestock)
+                      grass_frac -> 0.2 * grass_frac, livestock, landholder)
 
 end
 
@@ -77,6 +172,7 @@ function make_possible_holding(model)  #, available_cells)
 
     possible_plot = [(x, y) for x in xmin:xmax for y in ymin:ymax]
 end
+
 
 # Helper function to make a valid holding.
 function make_valid_holding(available_cells)
